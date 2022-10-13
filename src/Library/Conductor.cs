14c8d5b6627848733d2f3167739c484c2c1f874b
MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
2022-10-13 18:17:25 -0300

    ci valida

--- src/Library/Conductor.cs
+++ src/Library/Conductor.cs
@@ -3,7 +3,7 @@ namespace Library{
     public class Conductor : Usuario{
         public string Vehiculo { get; set; }
         public string Bio { get; set; }
-        public Conductor(string name, string lastname, int ci, string vehiculo, string bio, int calification=0 ) : base(name, lastname,  ci,  calification=0 )
+        public Conductor(string name, string lastname, string ci, string vehiculo, string bio, int calification=0 ) : base(name, lastname,  ci,  calification=0 )
         {
             this.Vehiculo = vehiculo;
             this.Bio = bio;
--- src/Library/Pasajero.cs
+++ src/Library/Pasajero.cs
@@ -1,6 +1,6 @@
 using System;
 namespace Library{
     public class Pasajero : Usuario{
-        public Pasajero(string name, string lastname, int ci, int calification=0 ) : base(name, lastname,  ci,  calification=0 ){}
+        public Pasajero(string name, string lastname, string ci, int calification=0 ) : base(name, lastname,  ci,  calification=0 ){}
     }
 }
