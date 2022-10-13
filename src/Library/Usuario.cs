4959ac827b41621a01919f530ac13fb7a1ccbd06
MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
2022-10-13 18:17:33 -0300

    ci valida

--- src/Library/Usuario.cs
+++ src/Library/Usuario.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace Library;
 public class Usuario{
@@ -7,10 +8,21 @@ public class Usuario{
     public int Ci { get; set; }
 
     public int Calification {get; set;}
-    public Usuario (string name, string lastname, int ci, int calification=0 ){
+    public Usuario (string name, string lastname, string ci, int calification=0 ){
         this.Name = name;
         this.Lastname = lastname;
-        this.Ci = ci;
+        this.Ci = ValidarCedula(ci);
         this.Calification = calification;
+    }
+    private int ValidarCedula (string ci)
+    {
+        ci = Regex.Replace(ci, @"[^\d]","" );
+        if (Regex.IsMatch(ci, @"\d{8}")){
+            return int.Parse(ci);
+        }
+        else{
+            throw new ArgumentException ("Cédula invalida");
+        }
+
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,11 +11,11 @@ namespace PII_Herencia
             
 
 
-            Usuario pasajero1 = new Pasajero("Pedro", "Sanchez", 55369874);
-            Usuario pasajero2 = new Pasajero("Juan", "Perez", 45879534);
-            Usuario pasajero3 = new Pasajero("Julia", "Rodriguez", 55889874);
-            Usuario conductor1 = new Conductor("Pedro", "Sanchez", 55369874, "Auto", "Hola un gusto");
-            Usuario conductorPool1 = new ConductorPool("Pedro", "Sanchez", 55369874, "Auto", "Hola un gusto", 3);
+            Usuario pasajero1 = new Pasajero("Pedro", "Sanchez", "55369874");
+            Usuario pasajero2 = new Pasajero("Juan", "Perez", "45879534");
+            Usuario pasajero3 = new Pasajero("Julia", "Rodriguez", "55889874");
+            Usuario conductor1 = new Conductor("Pedro", "Sanchez", "5.536.987-4", "Auto", "Hola un gusto");
+            Usuario conductorPool1 = new ConductorPool("Pedro", "Sanchez", "55369874", "Auto", "Hola un gusto", 3);
             
             
             UcuRideShare.Instance.Usuarios.Add(conductor1);
